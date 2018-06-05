38a9a5c4d58b1afaf17b079d9c23c10710b1177d
Raman Deep <deep.raman85@gmail.com>
2018-06-05 15:02:44 +0200

    Adapted script to accept remote host parameter and changed the output.

--- check_certs.sh
+++ check_certs.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-TARGET="mysite.example.net"; 
-RECIPIENT="hostmaster@mysite.example.net";
-DAYS=7;
-echo "checking if $TARGET expires in less than $DAYS days";
+TARGET="$1"
+DAYS=15;
 expirationdate=$(date -d "$(: | openssl s_client -connect $TARGET:443 -servername $TARGET 2>/dev/null \
                               | openssl x509 -text \
                               | grep 'Not After' \
-                              |awk '{print $4,$5,$7}')" '+%s'); 
+                              |awk '{print $4,$5,$7}')" '+%s');
 in7days=$(($(date +%s) + (86400*$DAYS)));
+#expirationdate=$(($(date +%s) + (86400*5)))
 if [ $in7days -gt $expirationdate ]; then
-    echo "KO - Certificate for $TARGET expires in less than $DAYS days, on $(date -d @$expirationdate '+%Y-%m-%d')" \
-    | mail -s "Certificate expiration warning for $TARGET" $RECIPIENT ;
+    echo "KO - Certificate for $TARGET expires in less than $DAYS days, on $(date -d @$expirationdate '+%Y-%m-%d')"
+    exit 1
 else
-    echo "OK - Certificate expires on $expirationdate";
-fi;
+    echo "OK - Certificate expires on $(date -d @$expirationdate '+%Y-%m-%d')";
+    exit 0
+fi
